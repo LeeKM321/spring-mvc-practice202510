46c411049fe5ab819bea0f698e98437835b77834
LeeKM321 <stephen4951@gmail.com>
2025-10-31 19:01:43 +0900

    게시글 상세보기 요청 작성. 게시글 검색 처리 작성 중

--- src/main/java/com/codeit/blog/repository/MemoryCommentRepository.java
+++ src/main/java/com/codeit/blog/repository/MemoryCommentRepository.java
@@ -4,16 +4,18 @@ import com.codeit.blog.entity.Comment;
 import com.codeit.blog.entity.Post;
 import org.springframework.stereotype.Repository;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Repository
 public class MemoryCommentRepository implements CommentRepository {
 
-    private final Map<Long, Post> store = new ConcurrentHashMap<>();
+    private final Map<Long, Comment> store = new ConcurrentHashMap<>();
     private final AtomicLong sequence = new AtomicLong(1L);
 
 
@@ -34,6 +36,20 @@ public class MemoryCommentRepository implements CommentRepository {
 
     @Override
     public List<Comment> findByPostId(Long postId) {
-        return List.of();
+        return store.values().stream()
+                .filter(comment -> comment.getPostId().equals(postId))
+                .sorted(Comparator.comparing(Comment::getCreateAt).reversed())
+                .collect(Collectors.toList());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
